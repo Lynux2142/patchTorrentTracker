baf2cac5f57b37fb575f55924be4a555ba8b8fa2
Lucas Guillerot lguiller <lguiller@student.42.fr>
2023-03-05 15:13:13 +0100

    ajout de la possibilite de choisir le nouveau tracker

--- patchTorrentTracker.py
+++ patchTorrentTracker.py
@@ -12,14 +12,18 @@ def generate_tracker():
 
 
 if __name__ == "__main__":
-    if len(argv) != 2:
-        raise BadUserInputError("ERROR: Bad input, need torrent path")
+    if len(argv) == 2:
+        new_tracker = bytes(generate_tracker(), "utf-8")
+    elif len(argv) == 3:
+        new_tracker = bytes(argv[2], "utf-8")
+    else:
+        raise BadUserInputError("ERROR: Bad input\nUsage: python3 patchTorrentTracker.py [path/to/torrent/file] ([new_tracker])")
     with open(argv[1], "r+b") as torrent:
-        fake_tracker = bytes(generate_tracker(), "utf-8")
         data = torrent.read()
-        current_tracker = bytes(re.search("/([A-Za-z0-9]{32})/", data[0:100].decode("utf-8")).group(1), "utf-8")
-        data = data.replace(current_tracker, fake_tracker)
+        old_tracker = bytes(re.search("/([A-Za-z0-9]{32})/", data[0:100].decode("utf-8")).group(1), "utf-8")
+        data = data.replace(old_tracker, new_tracker)
         torrent.seek(0)
         torrent.write(data)
         torrent.truncate()
-        print(f"New tracker: {fake_tracker.decode('utf-8')}")
+        print(f"Old tracker: {old_tracker.decode('utf-8')}")
+        print(f"New tracker: {new_tracker.decode('utf-8')}")
