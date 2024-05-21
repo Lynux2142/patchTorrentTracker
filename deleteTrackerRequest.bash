ef7fa8f7e9f8992eba1a6956ee6d49d326a53bd7
Lucas Guillerot lguiller <lguiller@student.42.fr>
2024-05-21 23:54:09 +0200

    add -n to echo command to remove \n at the end of the url_encoded and replace tracker_url by url_encoded in curl command

--- deleteTrackerRequest.bash
+++ deleteTrackerRequest.bash
@@ -9,6 +9,5 @@
 
 sleep 2
 tracker_url=$(sed -n 's|.*\(http[^ ]*announce\).*|\1|p' $1)
-url_encoded=$(echo $tracker_url | jq --slurp --raw-input --raw-output @uri)
-echo $tracker_url
-curl 'http://localhost:8080/api/v2/torrents/removeTrackers' -X POST --data-raw "hash=$2&urls=$tracker_url"
+url_encoded=$(echo -n $tracker_url | jq --slurp --raw-input --raw-output @uri)
+curl 'http://localhost:8080/api/v2/torrents/removeTrackers' -X POST --data-raw "hash=$2&urls=$url_encoded"
