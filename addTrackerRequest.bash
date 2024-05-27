e0b8c1c1185947cde953bd8efd1268f648f2fcfd
Lucas Guillerot lguiller <lguiller@student.42.fr>
2024-05-27 09:54:41 +0200

    switch from sed to grep and change the regex expression, add script to add again the tracker when torrent finished to download

---
+++ addTrackerRequest.bash
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# $1 path to the torrent directory
+# $2 torrent name
+# $3 is the hash of the torrent
+
+# copy this script to /config folder in qbittorrent container
+# add the following line to qbittorrent settings in "on torrent added" section
+# /config/deleteTrackerRequest.bash "/config/qBittorrent/BT_backup/%I.fastresume" %I
+
+tracker_url=$(head -n 1 "$1/$2.tracker")
+url_encoded=$(echo -n $tracker_url | jq --slurp --raw-input --raw-output @uri)
+curl 'http://localhost:8080/api/v2/torrents/addTrackers' -X POST --data-raw "hash=$3&urls=$url_encoded"
--- deleteTrackerRequest.bash
+++ deleteTrackerRequest.bash
@@ -1,14 +1,16 @@
 #!/bin/bash
 
-# $1 is the file path of the torrent file
-# $2 is the hash of the torrent
-# $3 Sleep duartion before deleting the torrent tracker
+# $1 path to the torrent directory
+# $2 torrent name
+# $3 hash of the torrent
+# $4 Sleep duartion before deleting the torrent tracker
 
 # copy this script to /config folder in qbittorrent container
 # add the following line to qbittorrent settings in "on torrent added" section
 # /config/deleteTrackerRequest.bash "/config/qBittorrent/BT_backup/%I.fastresume" %I 4
 
-sleep $3
-tracker_url=$(sed -n 's|.*\(http[^ ]*announce\).*|\1|p' $1)
+sleep $4
+tracker_url=$(grep -a -Po "https?:\/\/[^ ]*\/[a-zA-Z0-9]{32}\/announce" "$1/$2")
+echo -n $tracker_url > "$1/$2.tracker"
 url_encoded=$(echo -n $tracker_url | jq --slurp --raw-input --raw-output @uri)
-curl 'http://localhost:8080/api/v2/torrents/removeTrackers' -X POST --data-raw "hash=$2&urls=$url_encoded"
+curl 'http://localhost:8080/api/v2/torrents/removeTrackers' -X POST --data-raw "hash=$3&urls=$url_encoded"
